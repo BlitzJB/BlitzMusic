e8b927dd1bf3ef1b7742d0ed9adfa2a47dc3ec26
BlitzJB <blitz04.dev@gmail.com>
2023-11-02 20:21:06 +0530

    fix: be more explicit with the cachine

--- public/pwa/service-worker.js
+++ public/pwa/service-worker.js
@@ -2,9 +2,16 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('my-cache').then((cache) => {
             return cache.addAll([
-                '/',
                 '/pwa/manifest.json',
-                // Add other assets you want to cache here.
+                '/icons/loop.svg',
+                '/icons/loopone.svg',
+                '/icons/noloop.svg',
+                '/icons/pause.svg',
+                '/icons/play.svg',
+                '/icons/queue.svg',
+                '/icons/searchicon.svg',
+                '/icons/share.svg',
+                '/icons/unautheduser.svg',
             ]);
         })
     );
